bfb239591c9d068136f7c504c6113375ab6cb8b6
healplease <gavaalex2012@gmail.com>
2022-01-21 10:17:34 +0200

    method post

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,7 +4,7 @@ from config import Config
 
 main_bp = Blueprint("bot", __name__)
 
-@main_bp.route(f"/{Config.TELEGRAM_API_TOKEN}")
+@main_bp.route(f"/{Config.TELEGRAM_API_TOKEN}", methods=["POST"])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data(as_text=True)
